0635bd9171cd2b29f705a7bc8668c41a2f9771b2
PaStar98 <startekp98@gmail.com>
2022-11-11 18:45:14 +0100

    remove: unnecessary classes

--- src/MonsterOrc.java
+++
@@ -1,8 +0,0 @@
-public class MonsterOrc {
-    //test committa moj branch
-
-    String name = "Orc";
-    int hp = 200;
-    int[] weakAttack = {7,8,9,10};
-    int[] strongAttack = {15,16,17,18,19};
-}
